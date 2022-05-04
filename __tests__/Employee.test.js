f3c05e1e10701ea085939677f876cee4c004e4e0
Candice Hall <hall.candice@outlook.com>
2022-05-04 18:37:03 -0400

    finished tests, properties and methods for Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,28 @@
-const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee.js');
 
 test ('creates an employee object', ()=>{
     const employee = new Employee("Emily");
+    // employee.id = '3';
+    employee.email = 'email@email.com';
     
     expect(employee.name).toBe('Emily');
-    expect(employee.id).toEqual(expect.any(Number))
     expect(employee.email).toContain('@')
+})
+
+test ('function that will return name', ()=>{
+    const employee = new Employee("Emily");
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name))
+})
+
+test ("function that returns employee's email", ()=>{
+    const employee = new Employee("Emily");
+    employee.email = 'email@email.com';
+
+    expect (employee.getEmail()).toEqual(expect.stringContaining(employee.email))
+})
+test ("Function that returns employee's role", ()=>{
+    const employee = new Employee("Emily");
+
+    expect (employee.getRole()).toEqual(expect.stringContaining(employee.constructor.name))
 })
